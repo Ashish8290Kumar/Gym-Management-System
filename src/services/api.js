2310dcd9f567e18167bd272bb138da649b96b9ef
ashish <ashish8290kumar@gmail.com>
2026-07-25 22:04:21 +0530

    Update backend URL to live server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8090/api',
+    baseURL: 'https://gym-management-system-backend-2zp3.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
