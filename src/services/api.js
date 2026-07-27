04f05fd69ddd2bc704c0cfc38646e60fa40f45ca
ashish <ashish8290kumar@gmail.com>
2026-07-27 09:02:26 +0530

    Initial commit with Render backend API setup

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,20 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://onrender.com',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: 'https://gym-management-system-backend-2zp3.onrender.com', 
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
-
 API.interceptors.request.use((config) => {
-    const token = localStorage.getItem('gym_token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem('gym_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 }, (error) => {
-    return Promise.reject(error);
+  return Promise.reject(error);
 });
 
 export default API;
